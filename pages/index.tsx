7963f58a342e7e3ad75f5af8ccb6a71e25b49ce8
Long Bui <xuanlong22000@gmail.com>
2023-09-27 21:40:06 +0700

    add title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
 import Form from "@/components/Form";
 import Header from "@/components/Header";
 import PostsFeed from "@/components/posts/PostsFeed";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Twitter Clone</title>
+      </Head>
       <Header label="Home" />
       <Form placeholder="What are you thinking ?" />
       <PostsFeed />

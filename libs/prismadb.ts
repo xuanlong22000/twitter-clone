73ac7f67c5a83bf8b24a23828ed4adad158c8b9d
Long Bui <xuanlong22000@gmail.com>
2023-09-29 22:48:29 +0700

    change prisma.ts

--- libs/prismadb.ts
+++ libs/prismadb.ts
@@ -4,8 +4,13 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
-const client = globalThis.prisma || new PrismaClient();
+// const client = globalThis.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalThis.prisma = client;
+// if (process.env.NODE_ENV !== "production") globalThis.prisma = client;
 
-export default client;
+// export default client;
+
+const prisma = global.prisma || new PrismaClient({ log: ["info"] });
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+
+export default prisma;
